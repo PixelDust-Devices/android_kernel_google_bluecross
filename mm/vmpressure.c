5760dc97f644ac95120e8fa2bfe8d830be2f3e75
Vinayak Menon <vinmenon@codeaurora.org>
2015-08-19 16:16:39 +0530

    mm: vmpressure: account allocstalls only on higher pressures
    At present any vmpressure value is scaled up if the pages are
    reclaimed through direct reclaim. This can result in false
    vmpressure values. Consider a case where a device is booted up
    and most of the memory is occuppied by file pages. kswapd will
    make sure that high watermark is maintained. Now when a sudden
    huge allocation request comes in, the system will definitely
    have to get into direct reclaims. The vmpressures can be very low,
    but because of allocstall accounting logic even these low values
    will be scaled to values nearing 100. This can result in
    unnecessary LMK kills for example. So define a tunable threshold
    for vmpressure above which the allocstalls will be accounted.
    
    Change-Id: Idd7c6724264ac89f1f68f2e9d70a32390ffca3e5
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -178,8 +178,12 @@ out:
 static unsigned long vmpressure_account_stall(unsigned long pressure,
 				unsigned long stall, unsigned long scanned)
 {
-	unsigned long scale =
-		((vmpressure_scale_max - pressure) * stall) / scanned;
+	unsigned long scale;
+
+	if (pressure < allocstall_threshold)
+		return pressure;
+
+	scale = ((vmpressure_scale_max - pressure) * stall) / scanned;
 
 	return pressure + scale;
 }
