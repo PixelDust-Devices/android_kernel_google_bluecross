a7f04e11cc71662fac715b3aa0223324729ad3b9
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-08-20 12:59:59 +0530

    sched: walt: Optimize task_util()
    task_util() for WALT is currently defined as
    
    p->ravg.demand / (sched_ravg_window >> SCHED_CAPACITY_SHIFT);
    
    This math is required to scale the task demand to 1024 scale.
    task_util() is used many times in task placement. So the calls
    to this can be optimized by caching the scaled value when task
    demand is calculated.
    
    Change-Id: I0c170a10704ae3e8fe4e9f271e8e65c3923075e5
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1517,6 +1517,8 @@ struct ravg {
 	 *
 	 * 'busy_buckets' groups historical busy time into different buckets
 	 * used for prediction
+	 *
+	 * 'demand_scaled' represents task's demand scaled to 1024
 	 */
 	u64 mark_start;
 	u32 sum, demand;
@@ -1527,6 +1529,7 @@ struct ravg {
 	u16 active_windows;
 	u32 pred_demand;
 	u8 busy_buckets[NUM_BUSY_BUCKETS];
+	u16 demand_scaled;
 };
 
 struct sched_entity {
