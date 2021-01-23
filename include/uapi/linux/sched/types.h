146cd7a4294fd114d09c8f278292e9b9065df32d
spezi77 <spezi7713@gmx.net>
2021-01-24 00:29:15 +0100

    uapi/linux/sched: Add missing header for simple_lmk.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ include/uapi/linux/sched/types.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_SCHED_TYPES_H
+#define _UAPI_LINUX_SCHED_TYPES_H
+
+#include <linux/types.h>
+
+#endif /* _UAPI_LINUX_SCHED_TYPES_H */
